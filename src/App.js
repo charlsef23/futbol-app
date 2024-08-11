ea6660b3cad13dff9e6355b0f7645dec1d906b0a
Carlos Esteve <charlsef66@gmail.com>
2024-08-11 16:08:49 +0200

    0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Matches from './Matches';
-import Statistics from './Statistics';
-import News from './News';
-import MatchesCalendar from './MatchesCalendar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Matches from './components/Matches';
+import Statistics from './components/Statistics';
+import News from './components/News';
+import MatchesCalendar from './components/MatchesCalendar';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/matches" element={<Matches />} />
-          <Route path="/statistics" element={<Statistics />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/calendar" element={<MatchesCalendar />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/matches" element={<Matches />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/calendar" element={<MatchesCalendar />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
